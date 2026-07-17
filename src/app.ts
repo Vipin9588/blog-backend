3adfaa8684eb182e2b8a0831e3f894ac5aac2664
vipin <vipinkumar.ak40@gmail.com>
2026-07-17 16:17:00 +0530

    chore initialize backend project

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dontenv from 'dotenv';
+import path from 'path';
+const app = express();
+
+const envPath =  path.resolve(process.cwd(),".env")
+
+dontenv.config({path:envPath,debug:true,encoding:"utf8",quiet:true})
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the page")
+})
+
+export default app;
