eb07cb27c781959b502d9dbda1a08c4f5a27ab80
vipin <vipinkumar.ak40@gmail.com>
2026-09-23 00:49:04 +0530

    Adding google login Route

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { createUserController } from '#/controllers/auth/new-user.controllers.js';
 import { loginUserController } from '#/controllers/auth/user-login.controller.js';
 import { logout } from '#/controllers/auth/logout-user.controller.js';
+import {googleLoginController} from '#/controllers/auth/google-login.js';
 
 const router = express.Router();
 
 router.post('/register',createUserController);
 router.post('/login',loginUserController);
+router.post('/google-login',googleLoginController);
 router.post('/logout',logout);
 
 export default router; 
